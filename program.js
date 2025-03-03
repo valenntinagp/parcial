23945c3ccec8bfab18f51e2ae653bf5821dc0f82
Valentina Gonzalez <valenntina.gp@gmail.com>
2025-03-03 14:23:56 -0500

    v0.4

--- program.js
+++ program.js
@@ -13,7 +13,11 @@ async function loadPolygon(){
     L.geoJSON(myPolygon,
         {
             style:{
-                color: 'blue'
+                color: 'blue',
+                weight: 1,
+                fillColor: 'blue',
+                fillOpacity: 0.1,
+                
             }
         }
     ).addTo(map);
@@ -27,8 +31,15 @@ async function loadPoint(){
     
     L.geoJSON(myPoint,
         {
-            style:{
-                color: 'blue'
+            pointToLayer: (feature, latlong)=>{                    
+                
+                return L.circleMarker(latlong,{
+                    radius:4,
+                    fillColor:'green',
+                    weight:1,
+                    opacity:2,
+                    fillOpacity: 1,
+                })
             }
         }
     ).addTo(map);
@@ -36,6 +47,103 @@ async function loadPoint(){
 
 loadPoint();
 
-let btnTrees = document.getElementById('btnTrees');
+let btnTrees = document.getElementById("btnTrees");
 
-btnTrees.addEventListener('click', ()=> alert("hola"));
+btnTrees.addEventListener('click',
+    async function () {
+        let response= await fetch("arboles_timiza.geojson");
+        let datos= (await response.json());
+
+        //Agregar la capa al mapa
+        L.geoJSON(miPoint,
+            {
+                pointToLayer: (feature, latlong)=>{                    
+                    
+                    return L.circleMarker(latlong,{
+                        radius:3,
+                        fillColor:'green',
+                        weight:1,
+                        opacity:1,
+                        fillOpacity: 0.5,
+                    })
+                }
+            }
+        ).addTo(map);
+    }
+)
+
+let btnDistance= document.getElementById("btnDistance");
+
+btnDistance.addEventListener('click',
+    async function() {
+        let response= await fetch("arboles_timiza.geojson");
+        let datos= await response.json();
+        let trees= datos.features.map((myElement, index)=>({
+            id: index+1,
+            coordinates: myElement.geometry.coordinates
+        }));        
+
+        let distances=[];
+        trees.forEach( (treeA)=>{trees.forEach(
+
+            
+                (treeB)=>{
+                    if(treeA.id != treeB.id){
+                        let distance = turf.distance( 
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push(
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)                            
+                            ]
+                        )
+                }
+            }
+            )
+        }
+        )
+        generatePDF(distances, trees.lenght);
+    }
+)
+function generatePDF(distances, totalTrees){
+    let { jsPDF } = window.jspdf;
+    let documentPDF= new jsPDF();   
+    
+    documentPDF.text("REPORTE DE ÁRBOLES EN EL BARRIO TIMIZA", 10,10);
+
+    documentPDF.autoTable(
+        {
+            head: [['Árbol 1', 'Árbol 2', 'Distance']],
+            body: distances
+        }
+    );
+    documentPDF.save("Timiza.pdf")
+}
+btnSiniestros.addEventListener('click', 
+    async function(){
+        let response = await fetch("siniestros_timiza.geojson");
+        let datos = (await response.json());
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{
+
+                    return L.circleMarker(latlong, {
+                        radius:3,
+                        fillColor:'red',
+                        weight:1,
+                        opacity:0,
+                        fillOpacity: 0.5,
+                    })
+
+                }
+            }
+        ).addTo(map);
+
+    }
+    
+)
